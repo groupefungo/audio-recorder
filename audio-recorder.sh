d31437a69cc1df68819bae409f68ab63acffadfb
Rémi Gagnon <rem.gagnon@gmail.com>
2017-06-14 11:33:34 -0400

    updates with timestamp

--- audio-recorder.sh
+++ audio-recorder.sh
@@ -5,30 +5,36 @@ TIMELIMIT="timelimit"
 LIVESTREAMER="livestreamer"
 FFMPEG="ffmpeg"
 
+DATE=`date +"%Y-%m-%d-%H-%M"`
+
 # parameters
 IN=$1
-OUT=$2
+OUT=$2-$DATE.mp3
 TIME=$3
+
+echo $DATE
+echo $OUT
+
 if [[ -z $TIME ]]; then TIME="3600"; fi
 
 if [ $# -lt 1 ]; then
-	echo -n "Usage: $0 url output [time]"
-	exit 0
+        echo -n "Usage: $0 url output [time]"
+        exit 0
 fi
 
 if [[ -z `which $TIMELIMIT` ]]; then
-	echo "timelimit not found" 1>&2
-	exit 1;
+        echo "timelimit not found" 1>&2
+        exit 1;
 fi
 
 if [[ -z `which $LIVESTREAMER` ]]; then
-	echo "livestreamer not found" 1>&2
-	exit 1;
+        echo "livestreamer not found" 1>&2
+        exit 1;
 fi
 
 if [[ -z `which $FFMPEG` ]]; then
-	echo "ffmpeg not found" 1>&2
-	exit 1;
+        echo "ffmpeg not found" 1>&2
+        exit 1;
 fi
 
 echo "START STREAMING"
